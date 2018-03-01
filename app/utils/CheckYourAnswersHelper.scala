ace43a74f6760e16115f18222da9cb23dd968374
Darryl Murphy <darrylmurphy@outlook.com>
2018-03-01 16:07:28 +0000

    Initial creation of directorAddress with index and shared address component

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -22,6 +22,10 @@ import viewmodels.AnswerRow
 
 class CheckYourAnswersHelper(userAnswers: UserAnswers) {
 
+  def directorAddress(index: Int): Option[AnswerRow] = userAnswers.get(identifiers.company.directorAddressId) map {
+    x => AnswerRow("directorAddress.checkYourAnswersLabel", s"${x.addressLine1} ${x.addressLine2}", false, controllers.register.company.routes.DirectorAddressController.onPageLoad(CheckMode, index).url)
+  }
+
   def directorDetails(index: Int): Option[AnswerRow] = userAnswers.get(identifiers.register.company.DirectorDetailsId(index)) map {
     x => AnswerRow("directorDetails.checkYourAnswersLabel", s"${x.firstName} ${x.lastName}", false, controllers.register.company.routes.DirectorDetailsController.onPageLoad(CheckMode, index).url)
   }
