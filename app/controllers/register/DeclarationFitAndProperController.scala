5f9f63b9686e57fccf49d140bdb3ac69dba1c3c8
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2018-04-10 11:00:45 +0100

    PODS-569 Screen and controller completed

--- app/controllers/register/DeclarationFitAndProperController.scala
+++ app/controllers/register/DeclarationFitAndProperController.scala
@@ -22,17 +22,54 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import controllers.actions._
 import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.{Journey, JourneyType}
+import forms.register.DeclarationFormProvider
+import identifiers.register.DeclarationFitAndProperId
+import models.NormalMode
+import play.api.data.Form
 import play.api.mvc.{Action, AnyContent}
+import utils.{Navigator, UserAnswers}
 import views.html.register.declarationFitAndProper
+
+import scala.concurrent.Future
 
 class DeclarationFitAndProperController @Inject()(appConfig: FrontendAppConfig,
                                          override val messagesApi: MessagesApi,
                                          authenticate: AuthAction,
                                          getData: DataRetrievalAction,
-                                         requireData: DataRequiredAction) extends FrontendController with I18nSupport {
+                                         requireData: DataRequiredAction,
+                                         navigator: Navigator,
+                                         formProvider: DeclarationFormProvider,
+                                         dataCacheConnector: DataCacheConnector,
+                                         journey: Journey) extends FrontendController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (authenticate andThen getData andThen requireData) {
-    implicit request =>
-      Ok(declarationFitAndProper(appConfig))
+  private val form: Form[Boolean] = formProvider()
+
+  def onPageLoad: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+      implicit request =>
+        journey.withJourneyType {
+          case JourneyType.Individual =>
+            Future.successful(Ok(declarationFitAndProper(appConfig, form, controllers.register.individual.routes.WhatYouWillNeedController.onPageLoad())))
+          case JourneyType.Company =>
+            Future.successful(Ok(declarationFitAndProper(appConfig, form, controllers.register.company.routes.WhatYouWillNeedController.onPageLoad())))
+        }
   }
+
+  def onSubmit: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        errors => journey.withJourneyType {
+          case JourneyType.Individual =>
+            Future.successful(BadRequest(declarationFitAndProper(appConfig, errors, controllers.register.individual.routes.WhatYouWillNeedController.onPageLoad())))
+          case JourneyType.Company =>
+            Future.successful(BadRequest(declarationFitAndProper(appConfig, errors, controllers.register.company.routes.WhatYouWillNeedController.onPageLoad())))
+        },
+        success => dataCacheConnector.save(request.externalId, DeclarationFitAndProperId, success).map { cacheMap =>
+          Redirect(navigator.nextPage(DeclarationFitAndProperId, NormalMode)(UserAnswers(cacheMap)))
+        }
+      )
+  }
+
+
 }
