00c43b92961fd7c5ff239ed0d0c229662e5e8e69
Madhura <madhuranakate@gmail.com>
2018-06-08 16:00:58 +0100

    Initial commit for IndividualContactAddress

---
+++ test/controllers/register/individual/IndividualContactAddressControllerSpec.scala
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.individual
+
+import audit.testdoubles.StubSuccessfulAuditService
+import audit.{AddressAction, AddressEvent}
+import play.api.data.Form
+import play.api.libs.json.JsString
+import uk.gov.hmrc.http.cache.client.CacheMap
+import utils.{FakeCountryOptions, FakeNavigator, UserAnswers}
+import connectors.FakeDataCacheConnector
+import controllers.actions._
+import play.api.test.Helpers._
+import forms.register.individual.IndividualContactAddressFormProvider
+import identifiers.register.individual.{IndividualContactAddressId, IndividualPreviousAddressId}
+import models.{Address, Index, NormalMode, TolerantAddress}
+import views.html.register.individual.individualContactAddress
+import play.api.libs.json._
+import controllers.ControllerSpecBase
+import forms.AddressFormProvider
+import play.api.mvc.Call
+import utils.countryOptions.CountryOptions
+import viewmodels.Message
+import viewmodels.address.ManualAddressViewModel
+import views.html.address.manualAddress
+
+class IndividualContactAddressControllerSpec extends ControllerSpecBase {
+
+  def onwardRoute: Call = controllers.routes.IndexController.onPageLoad()
+  def countryOptions: CountryOptions = new FakeCountryOptions(environment, frontendAppConfig)
+
+  val messagePrefix = "common.contactAddress"
+  val firstIndex = Index(0)
+
+  val formProvider = new AddressFormProvider(new FakeCountryOptions(environment, frontendAppConfig))
+  val form: Form[Address] = formProvider("error.country.invalid")
+
+  val viewmodel = ManualAddressViewModel(
+    postCall = routes.IndividualContactAddressController.onSubmit(NormalMode),
+    countryOptions = countryOptions.options,
+    title = Message(s"$messagePrefix.title"),
+    heading = Message(s"$messagePrefix.heading"),
+    secondaryHeader = Some("common.individual.secondary.heading"),
+    hint = Some(Message(s"$messagePrefix.lede"))
+  )
+
+  val fakeAuditService = new StubSuccessfulAuditService()
+
+  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyData) =
+    new IndividualContactAddressController(
+      frontendAppConfig,
+      messagesApi,
+      FakeDataCacheConnector,
+      new FakeNavigator(desiredRoute = onwardRoute),
+      FakeAuthAction,
+      dataRetrievalAction,
+      new DataRequiredActionImpl,
+      formProvider,
+      countryOptions,
+      fakeAuditService
+    )
+
+  def viewAsString(form: Form[_] = form): String = manualAddress(frontendAppConfig, form, viewmodel)(fakeRequest, messages).toString
+
+  "IndividualContactAddress Controller" must {
+
+    "return OK and the correct view for a GET" in {
+      val result = controller().onPageLoad(NormalMode)(fakeRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe viewAsString()
+    }
+
+    "populate the view correctly on a GET when the question has previously been answered" in {
+      val validData = Json.obj(IndividualContactAddressId.toString -> Address("value 1", "value 2", None, None, None, "GB"))
+      val getRelevantData = new FakeDataRetrievalAction(Some(validData))
+
+      val result = controller(getRelevantData).onPageLoad(NormalMode)(fakeRequest)
+
+      contentAsString(result) mustBe viewAsString(form.fill(Address("value 1", "value 2", None, None, None, "GB")))
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(
+        ("addressLine1", "value 1"),
+        ("addressLine2", "value 2"),
+        ("postCode", "NE1 1NE"),
+        "country" -> "GB"
+      )
+
+      val result = controller().onSubmit(NormalMode)(postRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(onwardRoute.url)
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", "invalid value"))
+      val boundForm = form.bind(Map("value" -> "invalid value"))
+
+      val result = controller().onSubmit(NormalMode)(postRequest)
+
+      status(result) mustBe BAD_REQUEST
+      contentAsString(result) mustBe viewAsString(boundForm)
+    }
+
+    "redirect to Session Expired" when {
+      "no existing data is found" when {
+        "GET" in {
+          val result = controller(dontGetAnyData).onPageLoad(NormalMode)(fakeRequest)
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+        }
+        "POST" in {
+          val postRequest = fakeRequest.withFormUrlEncodedBody()
+          val result = controller(dontGetAnyData).onSubmit(NormalMode)(postRequest)
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
+        }
+      }
+    }
+
+  }
+}
