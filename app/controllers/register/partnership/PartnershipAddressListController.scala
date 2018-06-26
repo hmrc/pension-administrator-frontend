5d5e6f8fa65dbfdd1e0139125670b278b0c32d69
Darryl Murphy <darrylmurphy@outlook.com>
2018-06-26 10:02:45 +0100

    PartnershipAddressList test setup

---
+++ app/controllers/register/partnership/PartnershipAddressListController.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.partnership
+
+import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import controllers.address.AddressListController
+import forms.address.AddressListFormProvider
+import javax.inject.Inject
+import models.{Index, Mode}
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+import utils.Navigator
+import utils.annotations.Partnership
+
+class PartnershipAddressListController @Inject()(
+                                                val cacheConnector: DataCacheConnector,
+                                                @Partnership val navigator: Navigator,
+                                                val appConfig: FrontendAppConfig,
+                                                val messagesApi: MessagesApi,
+                                                authenticate: AuthAction,
+                                                getData: DataRetrievalAction,
+                                                requireData: DataRequiredAction,
+                                                formProvider: AddressListFormProvider
+                                                ) extends AddressListController {
+
+  def onPageLoad(mode: Mode, index: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData) {
+    implicit request =>
+      ???
+  }
+
+  def onSubmit(mode: Mode, index: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData) {
+    implicit request =>
+      ???
+  }
+
+}
