4afb5030ce9c3b08720fa6f3ea8d8bf0729791e8
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2018-07-02 12:40:38 +0100

    Skeleton of partner contact details controller

---
+++ app/controllers/register/partnership/partners/PartnerContactDetailsController.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.partnership.partners
+
+import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.ContactDetailsController
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import javax.inject.Inject
+import models.{Index, Mode}
+import play.api.i18n.MessagesApi
+import play.api.mvc.AnyContent
+import play.api.mvc.Action
+import utils.Navigator
+import utils.annotations.Partnership
+
+class PartnerContactDetailsController @Inject()(
+                                                 val appConfig: FrontendAppConfig,
+                                                 val cacheConnector: DataCacheConnector,
+                                                 @Partnership val navigator: Navigator,
+                                                 val messagesApi: MessagesApi,
+                                                 authenticate: AuthAction,
+                                                 getData: DataRetrievalAction,
+                                                 requireData: DataRequiredAction
+                                               ) extends ContactDetailsController {
+
+  def onPageLoad(mode: Mode, index: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData) {
+    implicit request =>
+      ???
+  }
+
+  def onSubmit(mode: Mode, index: Index): Action[AnyContent] = (authenticate andThen getData andThen requireData) {
+    implicit request =>
+      ???
+  }
+
+}
