deaff2ffa5c7d31660a027ca9cfb3a89d06f3ead
Madhura <madhuranakate@gmail.com>
2018-07-06 09:42:21 +0100

    Code for AddPartners and generic add controller

---
+++ app/controllers/register/partnership/AddPartnerController.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.partnership
+
+import config.FrontendAppConfig
+import connectors.DataCacheConnector
+import controllers.Retrievals
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import controllers.register.company.AddEntityController
+import forms.register.AddEntityFormProvider
+import identifiers.register.partnership.AddPartnersId
+import javax.inject.Inject
+import play.api.data.Form
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+import utils.Navigator
+import utils.annotations.Partnership
+import viewmodels.{EntityViewModel, Message, Person}
+
+class AddPartnerController @Inject() (
+                             override val appConfig: FrontendAppConfig,
+                             override val messagesApi: MessagesApi,
+                             override val cacheConnector: DataCacheConnector,
+                             @Partnership override val navigator: Navigator,
+                             authenticate: AuthAction,
+                             getData: DataRetrievalAction,
+                             requireData: DataRequiredAction,
+                             formProvider: AddEntityFormProvider
+                           ) extends AddEntityController with Retrievals {
+
+  private val form: Form[Boolean] = formProvider()
+
+  private def viewmodel(partners: Seq[Person]) = EntityViewModel(
+         postCall = routes.AddPartnerController.onSubmit,
+         title = Message("addPartners.title"),
+         heading = Message("addPartners.heading"),
+         entities = partners,
+         maxLimit = appConfig.maxPartners,
+         entityType = Message("addPartners.entityType"),
+         subHeading = Some(Message("site.secondaryHeader"))
+       )
+
+  def onPageLoad: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      val partners: Seq[Person] = request.userAnswers.allPartnersAfterDelete
+        get(AddPartnersId, form, viewmodel(partners))
+  }
+
+  def onSubmit: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      val partners: Seq[Person] = request.userAnswers.allPartnersAfterDelete
+        post(AddPartnersId, form, viewmodel(partners))
+  }
+
+}
