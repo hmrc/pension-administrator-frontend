996774891745686c62da0cfff9e0034bd0138c57
suryabalasingh <suryabala.singh@digital.hmrc.gov.uk>
2018-09-06 09:06:12 +0100

    PODS-1528-Refactored save calls to ensure both name and email saves successfully

--- app/controllers/register/DeclarationFitAndProperController.scala
+++ app/controllers/register/DeclarationFitAndProperController.scala
@@ -113,11 +113,15 @@ class DeclarationFitAndProperController @Inject()(appConfig: FrontendAppConfig,
       )
   }
 
-  private def savePSANameAndEmail(answers: UserAnswers, psaId: String)(implicit hc: HeaderCarrier, request: DataRequest[AnyContent]) = {
-    getName(answers) map { name =>
-      psaNameCacheConnector.save(psaId, PsaNameId, name)
-      psaNameCacheConnector.save(psaId, PsaEmailId, getEmail(answers).getOrElse(""))
-    } getOrElse {
+  private def savePSANameAndEmail(answers: UserAnswers, psaId: String)(implicit hc: HeaderCarrier, request: DataRequest[AnyContent]): Future[Unit] = {
+    getName(answers).map { name =>
+      for {
+        _ <- psaNameCacheConnector.save(psaId, PsaNameId, name)
+        _ <- psaNameCacheConnector.save(psaId, PsaEmailId, getEmail(answers).getOrElse(""))
+      } yield {
+        ()
+      }
+    }.getOrElse {
       Logger.error("Could not retrieve PSA Name")
       Future.failed(PSANameNotFoundException())
     }
