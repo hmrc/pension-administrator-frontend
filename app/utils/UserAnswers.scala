f8b5e519ae77c19c700b1a44ad81ded6bbe807e2
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2018-10-23 17:15:48 +0100

    PODS-1370-Added the clean up logic

--- app/utils/UserAnswers.scala
+++ app/utils/UserAnswers.scala
@@ -24,6 +24,7 @@ import models.{Index, NormalMode, PersonDetails}
 import play.api.libs.json._
 import viewmodels.Person
 
+import scala.annotation.tailrec
 import scala.language.implicitConversions
 
 case class UserAnswers(json: JsValue = Json.obj()) {
@@ -127,6 +128,23 @@ case class UserAnswers(json: JsValue = Json.obj()) {
     JsLens.fromPath(id.path)
       .remove(json)
       .flatMap(json => id.cleanup(None, UserAnswers(json)))
+  }
+
+  def removeAllOf[I <: TypedIdentifier.PathDependent](ids: List[I]): JsResult[UserAnswers] = {
+
+    @tailrec
+    def removeRec[II <: TypedIdentifier.PathDependent](localIds: List[II], result: JsResult[UserAnswers]): JsResult[UserAnswers] = {
+      result match {
+        case JsSuccess(value, path) =>
+          localIds match {
+            case Nil => result
+            case id :: tail => removeRec(tail, result.flatMap(_.remove(id)))
+          }
+        case failure => failure
+      }
+    }
+
+    removeRec(ids, JsSuccess(this))
   }
 
 }
