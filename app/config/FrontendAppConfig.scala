f8564a34eddc534a0ec91ef79f259d4e2ec21eff
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-12-04 15:25:54 +0000

    PODS-1893 Add not found page

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -59,6 +59,7 @@ class FrontendAppConfig @Inject()(override val runModeConfiguration: Configurati
   lazy val tellHMRCCompanyChangesUrl: String = loadConfig("urls.companyChangesHMRC")
   lazy val registerSchemeUrl: String = loadConfig("urls.pensions-scheme-frontend.registerScheme")
   lazy val schemesOverviewUrl : String = loadConfig("urls.manage-pensions-frontend.schemesOverview")
+  lazy val managePensionsYourPensionSchemesUrl: String = loadConfig("urls.manage-pensions-frontend.yourPensionSchemes")
   lazy val locationCanonicalList: String = loadConfig("location.canonical.list.all")
   lazy val locationCanonicalListEUAndEEA: String = loadConfig("location.canonical.list.EUAndEEA")
   lazy val maxDirectors: Int = loadConfig("register.company.maxDirectors").toInt
