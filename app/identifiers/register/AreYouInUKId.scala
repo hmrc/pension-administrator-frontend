04575ac5cf24cb7d63ee3c81ba33e3b99827a1ec
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2018-12-06 17:37:45 +0000

    PODS-2045-Adding clean up in progress

--- app/identifiers/register/AreYouInUKId.scala
+++ app/identifiers/register/AreYouInUKId.scala
@@ -17,10 +17,13 @@
 package identifiers.register
 
 import identifiers._
-import identifiers.register.company.{BusinessDetailsId, CompanyAddressId}
-import identifiers.register.partnership.{PartnershipDetailsId, PartnershipRegisteredAddressId}
-import identifiers.register.individual.{IndividualAddressId, IndividualDateOfBirthId, IndividualDetailsCorrectId, IndividualDetailsId}
-import play.api.libs.json.JsResult
+import identifiers.register.company._
+import identifiers.register.company.directors.DirectorId
+import identifiers.register.individual._
+import identifiers.register.partnership._
+import identifiers.register.partnership.partners.PartnerId
+import models.PersonDetails
+import play.api.libs.json.{JsResult, JsSuccess}
 import utils.UserAnswers
 
 case object AreYouInUKId extends TypedIdentifier[Boolean] {
@@ -29,15 +32,54 @@ case object AreYouInUKId extends TypedIdentifier[Boolean] {
   override def cleanup(value: Option[Boolean], userAnswers: UserAnswers): JsResult[UserAnswers] = {
     value match {
       case Some(false) =>
-        userAnswers.removeAllOf(List(BusinessDetailsId, BusinessTypeId,
-          PartnershipDetailsId,
-          IndividualDetailsId, IndividualAddressId, IndividualDetailsCorrectId))
+        removePartnershipData(userAnswers).flatMap( answers =>
+          removeCompanyData(answers)).flatMap(answers =>
+          removeIndividualData(answers)).flatMap(
+            _.removeAllOf(List(BusinessTypeId, CompanyRegistrationNumberId, ConfirmCompanyAddressId, CompanyContactAddressPostCodeLookupId,
+              CompanyDetailsId, ConfirmPartnershipDetailsId, PartnershipVatId, PartnershipPayeId, IndividualDetailsCorrectId,
+              IndividualContactAddressListId, IndividualPreviousAddressPostCodeLookupId
+            ))
+          )
       case Some(true) =>
-        userAnswers.removeAllOf(List(BusinessDetailsId, NonUKBusinessTypeId, CompanyAddressId,RegisterAsBusinessId,
-          PartnershipDetailsId, PartnershipRegisteredAddressId,
-          IndividualDetailsId, IndividualAddressId))
+        removePartnershipData(userAnswers).flatMap(
+          removeCompanyData).flatMap(
+          removeIndividualData).flatMap(
+          _.removeAllOf(List(NonUKBusinessTypeId, CompanyAddressId, PartnershipRegisteredAddressId, IndividualDetailsId, IndividualAddressId))
+        )
       case _ =>
         super.cleanup(value, userAnswers)
     }
+  }
+
+  private def removeAllDirectorsOrPartners(personDetailsSeq: Seq[PersonDetails],
+                                           userAnswers: UserAnswers, id: TypedIdentifier[Nothing]): JsResult[UserAnswers] = {
+    if (personDetailsSeq.nonEmpty) {
+      userAnswers.remove(id)
+    } else {
+      JsSuccess(userAnswers)
+    }
+  }
+
+  private def removeIndividualData(userAnswers: UserAnswers) = {
+    userAnswers.removeAllOf(List(
+      IndividualAddressYearsId,
+      IndividualPreviousAddressListId, IndividualPreviousAddressId, IndividualContactDetailsId, IndividualDateOfBirthId,
+      IndividualSameContactAddressId
+    ))
+  }
+
+  private def removePartnershipData(userAnswers: UserAnswers): JsResult[UserAnswers] = {
+    userAnswers.removeAllOf(List(PartnershipDetailsId, PartnershipSameContactAddressId,
+      PartnershipContactAddressPostCodeLookupId, PartnershipContactAddressListId, PartnershipContactAddressId,
+      PartnershipAddressYearsId, PartnershipPreviousAddressId, PartnershipPreviousAddressPostCodeLookupId,
+      PartnershipPreviousAddressListId, PartnershipContactDetailsId, MoreThanTenPartnersId))
+      .flatMap(answers => removeAllDirectorsOrPartners(answers.allPartners, answers, PartnerId))
+  }
+
+  private def removeCompanyData(userAnswers: UserAnswers): JsResult[UserAnswers] = {
+    userAnswers.removeAllOf(List(BusinessDetailsId, CompanySameContactAddressId,
+      CompanyAddressListId, CompanyContactAddressId, CompanyContactAddressListId, CompanyAddressYearsId, CompanyPreviousAddressId,
+      CompanyPreviousAddressPostCodeLookupId, ContactDetailsId, MoreThanTenDirectorsId))
+      .flatMap(answers => removeAllDirectorsOrPartners(answers.allDirectors, answers, DirectorId))
   }
 }
