59e506074defb2e9710d81ddc5451950da31237b
madhuranakate <madhuranakate@gmail.com>
2019-02-04 17:02:48 +0000

    Refactored

--- app/controllers/Variations.scala
+++ app/controllers/Variations.scala
@@ -21,8 +21,11 @@ import identifiers.TypedIdentifier
 import identifiers.register.individual.{IndividualAddressChangedId, IndividualContactAddressId}
 import models.{Mode, UpdateMode}
 import models.requests.DataRequest
-import play.api.libs.json.{JsResult, JsSuccess, JsValue}
+import play.api.http.Status.OK
+import play.api.libs.json._
 import play.api.mvc.AnyContent
+import uk.gov.hmrc.crypto.PlainText
+import uk.gov.hmrc.http.HttpException
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import utils.UserAnswers
 
@@ -34,24 +37,27 @@ trait Variations extends FrontendController {
 
   implicit val ec = play.api.libs.concurrent.Execution.defaultContext
 
-//  private def setChangeFlag(id: TypedIdentifier[Boolean])(implicit request: DataRequest[AnyContent]): JsResult[UserAnswers] = {
-//    request.userAnswers.set(id)(true)
-//  }
-//
-//  private def updateElement[A](mode:Mode, id: TypedIdentifier[A])(implicit request: DataRequest[AnyContent]): JsResult[UserAnswers] = {
-//    if (mode == UpdateMode) {
-//      id match {
-//        case IndividualContactAddressId => setChangeFlag(IndividualAddressChangedId)
-//        case _ => JsSuccess(request.userAnswers)
-//      }
-//    } else {
-//      JsSuccess(request.userAnswers)
-//    }
-//  }
+  private def setChangeFlag(id: TypedIdentifier[Boolean])(implicit request: DataRequest[AnyContent]): Future[JsValue] = {
+    request.userAnswers.set(id)(true) match {
+      case JsSuccess(UserAnswers(updatedJson), _) => Future.successful(updatedJson)
+      case JsError(errors) => Future.failed(JsResultException(errors))
+    }
+  }
 
-//  private val changeFlagsDirectors = Map(
-//    IndividualContactAddressId -> IndividualAddressChangedId
-//  )
+  def updateElement[A](mode:Mode, id: TypedIdentifier[A])(implicit request: DataRequest[AnyContent]): Future[JsValue] = {
+    if (mode == UpdateMode) {
+      id match {
+        case IndividualContactAddressId => setChangeFlag(IndividualAddressChangedId)
+        case _ => Future.successful(request.userAnswers.json)
+      }
+    } else {
+      Future.successful(request.userAnswers.json)
+    }
+  }
+
+  /*private val changeFlagsDirectors = Map(
+    IndividualContactAddressId -> IndividualAddressChangedId
+  )
 
   def saveChangeFlag[A](mode:Mode, id: TypedIdentifier[A])(implicit request: DataRequest[AnyContent]): Option[Future[JsValue]] = {
 
@@ -67,5 +73,5 @@ trait Variations extends FrontendController {
         value = true
       )
     }
-  }
+  }*/
 }
