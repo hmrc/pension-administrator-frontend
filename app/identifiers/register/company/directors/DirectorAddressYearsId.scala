d8d436929c14ee1f6eeda7856dd85ed5d45f7ee3
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2019-03-18 10:35:27 +0000

    PODS-2436-Fixed the clea up of partner or director address years

--- app/identifiers/register/company/directors/DirectorAddressYearsId.scala
+++ app/identifiers/register/company/directors/DirectorAddressYearsId.scala
@@ -31,6 +31,8 @@ case class DirectorAddressYearsId(index: Int) extends TypedIdentifier[AddressYea
       case Some(AddressYears.OverAYear) =>
         userAnswers.set(DirectorsOrPartnersChangedId)(true).asOpt.getOrElse(userAnswers)
           .removeAllOf(List(DirectorPreviousAddressPostCodeLookupId(index), DirectorPreviousAddressListId(index), DirectorPreviousAddressId(index)))
+      case Some(AddressYears.UnderAYear) =>
+        userAnswers.set(IsDirectorCompleteId(index))(false)
       case _ => super.cleanup(value, userAnswers)
     }
   }
