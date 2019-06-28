fccfe2150f69b5b38365cad9dcac2827883efa66
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-06-28 13:31:56 +0100

    PODS-2757 More

--- test/utils/CheckYourAnswersHelperSpec.scala
+++ test/utils/CheckYourAnswersHelperSpec.scala
@@ -29,15 +29,23 @@ import viewmodels.{AnswerRow, Link}
 
 class CheckYourAnswersHelperSpec extends SpecBase {
 
+  private val reason = "don't have one"
   private val localDate = LocalDate.parse("28/06/2019", DateTimeFormatter.ofPattern("dd/MM/yyyy"))
   private val countryOptions = new FakeCountryOptions(environment, frontendAppConfig)
 
-  def cyaHelperMethod(codeToTest: CheckYourAnswersHelper => Seq[AnswerRow], json: JsObject, expectedResult: Seq[AnswerRow]): Unit = {
+  case class TestScenario(userAnswersJson: JsObject, expectedResult: Seq[AnswerRow], name:Option[String] = None) {
+    def describe(descr:String):String = name.map(_ + s" ($descr)").getOrElse(descr)
+  }
+
+  def cyaHelperMethod(codeToTest: CheckYourAnswersHelper => Seq[AnswerRow],
+                      dataExistingTests: Seq[TestScenario]): Unit = {
     val countryOptions = new FakeCountryOptions(environment, frontendAppConfig)
     val genCYAHelper = new CheckYourAnswersHelper(_: UserAnswers, countryOptions)
 
-    "respond correctly when user answers data exists" in {
-      codeToTest(genCYAHelper(UserAnswers(json))) mustBe expectedResult
+    dataExistingTests.foreach { scenario =>
+      scenario.describe("respond correctly when user answers data exists") in {
+        codeToTest(genCYAHelper(UserAnswers(scenario.userAnswersJson))) mustBe scenario.expectedResult
+      }
     }
 
     "respond correctly when user answers data does not exist" in {
@@ -47,47 +55,73 @@ class CheckYourAnswersHelperSpec extends SpecBase {
 
   "directorContactDetails" should {
     behave like cyaHelperMethod(_.directorDetails(0, NormalMode),
-      Json.obj(
-        CompanyDetailsId.toString -> CompanyDetails(None, None),
-        "directors" -> Json.arr(
-          Json.obj(
-            DirectorDetailsId.toString ->
-              PersonDetails("test first name", Some("test middle name"), "test last name", localDate)
-          )
-        )
-      ),
       Seq(
-        AnswerRow("cya.label.name",
-          Seq("test first name test last name"),
-          answerIsMessageKey = false,
-          Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/director-details"))),
-        AnswerRow("cya.label.dob",
-          Seq("28 June 2019"),
-          answerIsMessageKey = false,
-          Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/director-details"))))
+        TestScenario(
+          Json.obj(
+            CompanyDetailsId.toString -> CompanyDetails(None, None),
+            "directors" -> Json.arr(
+              Json.obj(
+                DirectorDetailsId.toString ->
+                  PersonDetails("test first name", Some("test middle name"), "test last name", localDate)
+              )
+            )
+          ),
+          Seq(
+            AnswerRow("cya.label.name",
+              Seq("test first name test last name"),
+              answerIsMessageKey = false,
+              Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/director-details"))),
+            AnswerRow("cya.label.dob",
+              Seq("28 June 2019"),
+              answerIsMessageKey = false,
+              Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/director-details"))))
+        )
+      )
     )
+  }
+
+  def yesNoExpectedResult(answer:String, row2label:String, row2answer:String):Seq[AnswerRow] = {
+    Seq(
+      AnswerRow("directorUniqueTaxReference.checkYourAnswersLabel",
+        Seq(answer),
+        answerIsMessageKey = true,
+        Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/unique-taxpayer-reference"))),
+      AnswerRow(row2label,
+        Seq(row2answer),
+        answerIsMessageKey = true,
+        Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/unique-taxpayer-reference"))))
   }
 
   "directorUniqueTaxReference" should {
     behave like cyaHelperMethod(_.directorUniqueTaxReference(0, NormalMode),
-      Json.obj(
-        CompanyDetailsId.toString -> CompanyDetails(None, None),
-        "directors" -> Json.arr(
-          Json.obj(
-            DirectorUniqueTaxReferenceId.toString ->
-              UniqueTaxReference.Yes("utr")
-          )
-        )
-      ),
       Seq(
-        AnswerRow("directorUniqueTaxReference.checkYourAnswersLabel",
-          Seq("Yes"),
-          answerIsMessageKey = true,
-          Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/unique-taxpayer-reference"))),
-        AnswerRow("directorUniqueTaxReference.checkYourAnswersLabel.utr",
-          Seq("utr"),
-          answerIsMessageKey = true,
-          Some(Link("/register-as-pension-scheme-administrator/register/company/directors/1/change/unique-taxpayer-reference"))))
+        TestScenario(
+          Json.obj(
+            CompanyDetailsId.toString -> CompanyDetails(None, None),
+            "directors" -> Json.arr(
+              Json.obj(
+                DirectorUniqueTaxReferenceId.toString ->
+                  UniqueTaxReference.Yes("utr")
+              )
+            )
+          ),
+          yesNoExpectedResult("Yes", "directorUniqueTaxReference.checkYourAnswersLabel.utr", "utr"),
+          Some("user answered yes")
+        ),
+        TestScenario(
+          Json.obj(
+            CompanyDetailsId.toString -> CompanyDetails(None, None),
+            "directors" -> Json.arr(
+              Json.obj(
+                DirectorUniqueTaxReferenceId.toString ->
+                  UniqueTaxReference.No(reason)
+              )
+            )
+          ),
+          yesNoExpectedResult("No", "directorUniqueTaxReference.checkYourAnswersLabel.reason", reason),
+          Some("user answered no")
+        )
+      )
     )
   }
 
