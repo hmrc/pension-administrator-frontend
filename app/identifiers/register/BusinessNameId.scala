8ed4f505af4ce7b3a155f2570a416379082b7b06
Madhura <madhuranakate@gmail.com>
2019-10-14 18:26:12 +0100

    Minor corrections

--- app/identifiers/register/BusinessNameId.scala
+++ app/identifiers/register/BusinessNameId.scala
@@ -17,15 +17,7 @@
 package identifiers.register
 
 import identifiers._
-import identifiers.register.adviser._
-import identifiers.register.company._
-import identifiers.register.company.directors.DirectorId
-import identifiers.register.individual._
-import identifiers.register.partnership._
-import identifiers.register.partnership.partners.PartnerId
-import play.api.libs.json.JsResult
-import utils.UserAnswers
 
-case object BusinessNameId extends TypedIdentifier[Boolean] {
+case object BusinessNameId extends TypedIdentifier[String] {
   override def toString: String = "businessName"
 }
