0081cc2918c40ff1d054dca14abd65e67ec62461
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-10-28 15:19:34 +0000

    PODS-3582 Add two pages and tests

---
+++ app/identifiers/register/partnership/PartnershipEmailId.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package identifiers.register.partnership
+
+import identifiers.TypedIdentifier
+import play.api.i18n.Messages
+import play.api.libs.json.JsPath
+import utils.UserAnswers
+import utils.checkyouranswers.{CheckYourAnswers, CheckYourAnswersPartnership, StringCYA}
+import viewmodels.{AnswerRow, Link, Message}
+
+case object PartnershipEmailId extends TypedIdentifier[String] {
+  self =>
+
+  override def path: JsPath = JsPath \ "partnershipContactDetails" \ PartnershipEmailId.toString
+
+  override def toString: String = "email"
+
+  implicit def cya(implicit messages: Messages): CheckYourAnswers[self.type] =
+    new CheckYourAnswersPartnership[self.type] {
+      private def label(ua: UserAnswers): String =
+        dynamicMessage(ua, "email.title")
+
+      private def hiddenLabel(ua: UserAnswers): Message =
+        dynamicMessage(ua, "email.visuallyHidden.text")
+
+      override def row(id: self.type)(changeUrl: Option[Link], userAnswers: UserAnswers): Seq[AnswerRow] = {
+        StringCYA[self.type](Some(label(userAnswers)), Some(hiddenLabel(userAnswers)))().row(id)(changeUrl, userAnswers)
+      }
+    }
+}
