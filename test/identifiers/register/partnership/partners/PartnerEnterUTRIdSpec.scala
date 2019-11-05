08402590ce1b67257b76d69b5f84aa0485fe5c78
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2019-11-05 16:48:58 +0000

    PODS-3559-Added utr pages

---
+++ test/identifiers/register/partnership/partners/PartnerEnterUTRIdSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package identifiers.register.partnership.partners
+
+import base.SpecBase
+import models._
+import models.requests.DataRequest
+import play.api.libs.json.Json
+import play.api.mvc.AnyContent
+import play.api.test.FakeRequest
+import utils.UserAnswers
+import utils.checkyouranswers.Ops._
+import viewmodels.{AnswerRow, Link, Message}
+
+class PartnerEnterUTRIdSpec extends SpecBase {
+
+  private val personDetails = PersonName("test first", "test last")
+  private val onwardUrl = "onwardUrl"
+
+  "Cleanup" when {
+    def answers: UserAnswers =
+      UserAnswers(Json.obj())
+        .set(PartnerNoUTRReasonId(0))("reason")
+        .asOpt
+        .value
+
+    "remove the data for `PartnerNoUTRReason`" in {
+      val result: UserAnswers = answers.set(PartnerEnterUTRId(0))(ReferenceValue("UTR", isEditable = true)).asOpt.value
+      result.get(PartnerNoUTRReasonId(0)) mustNot be(defined)
+    }
+  }
+
+  "cya" when {
+    def answers: UserAnswers =
+      UserAnswers()
+        .set(PartnerNameId(0))(personDetails).asOpt.value
+        .set(PartnerEnterUTRId(0))(value = ReferenceValue("test-UTR")).asOpt.value
+
+    "in normal mode" must {
+      "return answers rows with change links" in {
+        val answerRows =
+          Seq(
+            AnswerRow(Message("enterUTR.heading").withArgs(personDetails.fullName).resolve, Seq("test-UTR"), answerIsMessageKey = false,
+              Some(Link("site.change", onwardUrl)), Some(Message("enterUTR.visuallyHidden.text").withArgs(personDetails.fullName))
+            )
+          )
+        val request: DataRequest[AnyContent] = DataRequest(FakeRequest(), "id",
+          PSAUser(UserType.Organisation, None, isExistingPSA = false, None), answers)
+
+        PartnerEnterUTRId(0).row(Some(Link("site.change", onwardUrl)))(request, implicitly) must equal(answerRows)
+      }
+    }
+  }
+}
