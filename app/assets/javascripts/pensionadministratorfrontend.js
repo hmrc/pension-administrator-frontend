c670195cc5746c430a15dbf1b1c238d424feb2f1
ashwani1403 <ashwani.kumar@digital.hmrc.gov.uk>
2020-02-02 17:08:10 +0000

    PODS-3875: session logout changes

--- app/assets/javascripts/pensionadministratorfrontend.js
+++ app/assets/javascripts/pensionadministratorfrontend.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
             this.click();
         }
     });
-
+    window.GOVUK.timeoutDialog({
+        timeout: 900,
+        countdown: 120,
+        keepAliveUrl: '/register-as-pension-scheme-administrator/keep-alive',
+        signOutUrl: '/register-as-pension-scheme-administrator/logout',
+      });
   // =====================================================
   // Initialise show-hide-content
   // Toggles additional content based on radio/checkbox input state
