d3f0bb237a6a63a166877872ec7343743eaaa124
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-08-06 14:51:55 +0100

    PODS-4550 Done

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "simple-reactivemongo" % "7.30.0-play-26",
     "uk.gov.hmrc" %% "logback-json-logger" % "4.8.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.50.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template" % "5.55.0-play-26",
     "uk.gov.hmrc" %% "play-health" % "3.14.0-play-26",
     "uk.gov.hmrc" %% "play-ui" % "8.11.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "9.1.0-play-26",
@@ -19,7 +19,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.14.0",
     "uk.gov.hmrc" %% "play-language" % "4.3.0-play-26",
     "uk.gov.hmrc" %% "domain" % "5.9.0-play-26",
-    "uk.gov.hmrc" %% "play-whitelist-filter" % "3.1.0-play-26",
+    "uk.gov.hmrc" %% "play-whitelist-filter" % "3.4.0-play-26",
     "com.typesafe.play" %% "play-json-joda" % "2.6.10"
   )
 
