d5a993ccf5cf3235f41ea8b1293b6691608f6f29
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2023-01-13 11:06:47 +0000

    PODS-7937 dependency upgrade for bobby warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,19 +7,19 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "http-caching-client"            % "10.0.0-play-28",
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-28"     % "7.11.0",
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc"             % "3.32.0-play-28",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc"             % "6.2.0-play-28",
     "uk.gov.hmrc"                   %% "domain"                         % "8.1.0-play-28",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"           % "2.14.1"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                  % "3.2.14",
+    "org.scalatest"           %% "scalatest"                  % "3.2.15",
     "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.14.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
     "org.pegdown"             %  "pegdown"                    % "1.6.0",
     "org.jsoup"               %  "jsoup"                      % "1.15.3",
     "org.mockito"             %% "mockito-scala"              % "1.17.12",
-    "io.github.wolfendale"    %% "scalacheck-gen-regexp"      % "1.0.0",
+    "io.github.wolfendale"    %% "scalacheck-gen-regexp"      % "1.1.0",
     "com.github.tomakehurst"  %  "wiremock-jre8"              % "2.35.0",
     "com.vladsch.flexmark"    % "flexmark-all"                % "0.62.2"
   ).map(_ % "test")
