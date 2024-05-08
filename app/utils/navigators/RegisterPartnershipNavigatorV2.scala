2fb4f9e8bfc20a028959b8f20cb1535127767bfa
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2024-05-08 13:13:14 +0100

    PODS-9277: Fixed incorrect annotation
    Code:
    - The V2 annotation should have been used instead of V1, as this code was introduced in V2 changes with PODS-7071.

--- app/utils/navigators/RegisterPartnershipNavigatorV2.scala
+++ app/utils/navigators/RegisterPartnershipNavigatorV2.scala
@@ -219,10 +219,9 @@ class RegisterPartnershipNavigatorV2 @Inject()(countryOptions: CountryOptions) e
     }
 
   private def sameContactAddress(mode: Mode, answers: UserAnswers): Call = {
-    (answers.get(PartnershipSameContactAddressId), answers.get(AreYouInUKId)) match {
-      case (Some(true), _) => PartnershipAddressYearsController.onPageLoad(mode)
-      case (Some(false), Some(false)) => PartnershipContactAddressController.onPageLoad(mode)
-      case (Some(false), Some(true)) => PartnershipContactAddressPostCodeLookupController.onPageLoad(mode)
+    answers.get(PartnershipSameContactAddressId) match {
+      case Some(true) => PartnershipAddressYearsController.onPageLoad(mode)
+      case Some(false) => PartnershipContactAddressController.onPageLoad(mode)
       case _ => controllers.routes.SessionExpiredController.onPageLoad
     }
   }
