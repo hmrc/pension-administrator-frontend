22ab8e97028f5d3c290a677ead61b2c4c9852442
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2025-10-08 12:01:03 +0100

    DDCNL-11145: Updated deps and add config for govuk rebrand

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,14 +1,14 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "9.16.0"
+  private val bootstrapVersion = "10.2.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"             % "12.7.0",
-    "uk.gov.hmrc"                   %% "domain-play-30"                         % "10.0.0",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                   % "2.19.1"
+    "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "12.17.0",
+    "uk.gov.hmrc"                   %% "domain-play-30"             % "13.0.0",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0"
   )
 
   val test: Seq[ModuleID] = Seq(
