17fb0adde80c6bc304ecb08b47c279efb9be3cd9
Jessica Kay <114414761+jesskay2@users.noreply.github.com>
2025-11-05 09:16:24 +0000

    DDCNL-11234 | Migrate to new service navigation component (#917)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "12.19.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "12.20.0",
     "uk.gov.hmrc"                   %% "domain-play-30"             % "13.0.0",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.1"
   )
