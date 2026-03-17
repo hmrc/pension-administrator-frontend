5aae6a79511afda82002ec813707dd3fbaa1b1c4
emrawsy <27211889+emrawsy@users.noreply.github.com>
2026-03-17 17:15:57 +0000

    DDCNL-11561 individual check your answers to include new id and nav

--- app/controllers/register/individual/CheckYourAnswersController.scala
+++ app/controllers/register/individual/CheckYourAnswersController.scala
@@ -19,23 +19,26 @@ package controllers.register.individual
 import com.google.inject.{Inject, Singleton}
 import controllers.Retrievals
 import controllers.actions.{AllowAccessActionProvider, AuthAction, DataRequiredAction, DataRetrievalAction}
-import controllers.register.individual.routes._
-import identifiers.register.individual._
+import controllers.register.individual.routes.*
+import identifiers.register.individual.*
 import models.Mode
 import models.Mode.checkMode
+import models.admin.ukResidencyToggle
 import models.requests.DataRequest
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc._
+import play.api.mvc.*
+import uk.gov.hmrc.mongoFeatureToggles.services.FeatureFlagService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.annotations.{AuthWithIV, Individual}
-import utils.checkyouranswers.Ops._
+import utils.checkyouranswers.Ops.*
 import utils.countryOptions.CountryOptions
 import utils.dataCompletion.DataCompletion
+import utils.navigators.IndividualNavigatorV2
 import utils.{Enumerable, Navigator}
 import viewmodels.{AnswerSection, Link}
 import views.html.check_your_answers
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class CheckYourAnswersController @Inject()(
@@ -45,50 +48,71 @@ class CheckYourAnswersController @Inject()(
                                             requireData: DataRequiredAction,
                                             dataCompletion: DataCompletion,
                                             @Individual navigator: Navigator,
+                                            individualNavigatorV2: IndividualNavigatorV2,
                                             override val messagesApi: MessagesApi,
                                             implicit val countryOptions: CountryOptions,
                                             val controllerComponents: MessagesControllerComponents,
+                                            featureFlagService: FeatureFlagService,
                                             view: check_your_answers
                                           )(implicit val executionContext: ExecutionContext)
   extends FrontendBaseController with Retrievals with I18nSupport with Enumerable.Implicits {
 
   lazy val postUrl: Call = routes.CheckYourAnswersController.onSubmit()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (authenticate andThen allowAccess(mode) andThen getData andThen requireData) {
+  def onPageLoad(mode: Mode): Action[AnyContent] = (authenticate andThen allowAccess(mode) andThen getData andThen requireData).async {
     implicit request =>
-      val ua = request.userAnswers
-      (ua.get(IndividualDetailsId), ua.get(IndividualAddressId)) match {
-        case (Some(_), Some(_)) =>
-          loadCyaPage(mode)
-        case _ =>
-          Redirect(controllers.register.routes.RegisterAsBusinessController.onPageLoad())
+      featureFlagService.get(ukResidencyToggle).flatMap { ukResidency =>
+        val ua = request.userAnswers
+        (ua.get(IndividualDetailsId), ua.get(IndividualAddressId)) match {
+          case (Some(_), Some(_)) =>
+            Future.successful(loadCyaPage(mode, ukResidency.isEnabled))
+          case _ =>
+            Future.successful(Redirect(controllers.register.routes.RegisterAsBusinessController.onPageLoad()))
+        }
       }
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (authenticate andThen allowAccess(mode) andThen getData andThen requireData) {
+  def onSubmit(mode: Mode): Action[AnyContent] = (authenticate andThen allowAccess(mode) andThen getData andThen requireData).async {
     implicit request =>
-      val isDataComplete = dataCompletion.isIndividualComplete(request.userAnswers, mode)
-      if (isDataComplete) {
-        Redirect(navigator.nextPage(CheckYourAnswersId, mode, request.userAnswers))
+      featureFlagService.get(ukResidencyToggle).flatMap { ukResidency =>
+        val isDataComplete: Boolean = if(ukResidency.isEnabled) {
+          dataCompletion.isIndividualUKComplete(request.userAnswers, mode)
+        } else {
+          dataCompletion.isIndividualComplete(request.userAnswers, mode)
+        }
+        (isDataComplete, ukResidency.isEnabled) match {
+          case (true, true) => Future.successful(Redirect(individualNavigatorV2.nextPage(CheckYourAnswersId, mode, request.userAnswers)))
+          case (true, false) => Future.successful(Redirect(navigator.nextPage(CheckYourAnswersId, mode, request.userAnswers)))
+          case _ => Future.successful(loadCyaPage(mode, ukResidency.isEnabled))
+        }
+
+      }
+  }
+
+  private def loadCyaPage(mode: Mode, ukResidency: Boolean)(implicit request: DataRequest[AnyContent]): Result = {
+      val correctContactAddress = if (ukResidency) {
+        IndividualUKContactAddressId.row(Some(Link(IndividualContactAddressPostCodeLookupController.onPageLoad(checkMode(mode)).url)))
       } else {
-        loadCyaPage(mode)
+        IndividualContactAddressId.row(Some(Link(IndividualContactAddressPostCodeLookupController.onPageLoad(checkMode(mode)).url)))
       }
-  }
-
-  private def loadCyaPage(mode: Mode)(implicit request: DataRequest[AnyContent]): Result = {
-    val individualDetails = AnswerSection(None, Seq(
-      IndividualDetailsId.row(None),
-      IndividualDateOfBirthId.row(Some(Link(IndividualDateOfBirthController.onPageLoad(checkMode(mode)).url))),
-      IndividualAddressId.row(None),
-      IndividualSameContactAddressId.row(Some(Link(IndividualSameContactAddressController.onPageLoad(checkMode(mode)).url))),
-      IndividualContactAddressId.row(Some(Link(IndividualContactAddressPostCodeLookupController.onPageLoad(checkMode(mode)).url))),
-      IndividualAddressYearsId.row(Some(Link(IndividualAddressYearsController.onPageLoad(checkMode(mode)).url))),
-      IndividualPreviousAddressId.row(Some(Link(
-        controllers.register.individual.routes.IndividualPreviousAddressPostCodeLookupController.onPageLoad(checkMode(mode)).url))),
-      IndividualEmailId.row(Some(Link(IndividualEmailController.onPageLoad(checkMode(mode)).url))),
-      IndividualPhoneId.row(Some(Link(IndividualPhoneController.onPageLoad(checkMode(mode)).url)))
-    ).flatten)
-    val sections = Seq(individualDetails)
-    Ok(view(sections, postUrl, None, mode, dataCompletion.isIndividualComplete(request.userAnswers, mode)))
-  }
+      val individualDetails = AnswerSection(None, Seq(
+        IndividualDetailsId.row(None),
+        IndividualDateOfBirthId.row(Some(Link(IndividualDateOfBirthController.onPageLoad(checkMode(mode)).url))),
+        IndividualAddressId.row(None),
+        IndividualSameContactAddressId.row(Some(Link(IndividualSameContactAddressController.onPageLoad(checkMode(mode)).url))),
+        correctContactAddress,
+        IndividualAddressYearsId.row(Some(Link(IndividualAddressYearsController.onPageLoad(checkMode(mode)).url))),
+        IndividualPreviousAddressId.row(Some(Link(
+          controllers.register.individual.routes.IndividualPreviousAddressPostCodeLookupController.onPageLoad(checkMode(mode)).url))),
+        IndividualEmailId.row(Some(Link(IndividualEmailController.onPageLoad(checkMode(mode)).url))),
+        IndividualPhoneId.row(Some(Link(IndividualPhoneController.onPageLoad(checkMode(mode)).url)))
+      ).flatten)
+      val sections = Seq(individualDetails)
+      val dataComplete = if(ukResidency) {
+        dataCompletion.isIndividualUKComplete(request.userAnswers, mode)
+      } else {
+        dataCompletion.isIndividualComplete(request.userAnswers, mode)
+      }
+      Ok(view(sections, postUrl, None, mode, dataComplete))
+    }
 }
